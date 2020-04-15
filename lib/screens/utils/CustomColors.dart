1b4a6088de254a88482c1e21aacfbc6d9d0d4482
Muthu Thavamani <muthucs@outlook.com>
2020-04-15 12:42:59 +0530

    added light grey color

--- lib/screens/utils/CustomColors.dart
+++ lib/screens/utils/CustomColors.dart
@@ -5,6 +5,7 @@ class CustomColors {
   static const Color mfinButtonGreen = Color(0xFF34D578);
   static const Color mfinFadedButtonGreen = Color(0xFF69F0AE);
   static const Color mfinGrey = Color(0xFFAFAFAF);
+  static const Color mfinLightGrey = Color(0xFFF3F3F3);
   static const Color mfinWhite = Color(0xFFFFFFFF);
 
   static const Color mfinAlertRed = Color(0xFFDD2C00);
