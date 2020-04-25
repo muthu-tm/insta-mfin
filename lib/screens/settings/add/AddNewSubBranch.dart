2d7900452e787bcd8a2b95c439e4d9cc7359b34a
Muthu Thavamani <muthucs@outlook.com>
2020-04-25 12:59:14 +0530

    screens for add branch and add subbranch; incomplete!

---
+++ lib/screens/settings/add/AddNewSubBranch.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddSubBranch extends StatefulWidget {
+  @override
+  _AddSubBranchState createState() => _AddSubBranchState();
+}
+
+class _AddSubBranchState extends State<AddSubBranch> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
