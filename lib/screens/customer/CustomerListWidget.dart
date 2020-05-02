8c7de03f03f1d87be08b88498da42c9c25c9d186
Muthu Thavamani <muthucs@outlook.com>
2020-05-02 21:55:05 +0530

    introduced slidable for customer module

--- lib/screens/customer/CustomerListWidget.dart
+++ lib/screens/customer/CustomerListWidget.dart
@@ -9,9 +9,11 @@ import 'package:instamfin/screens/utils/CustomSnackBar.dart';
 import 'package:instamfin/screens/utils/IconButton.dart';
 import 'package:instamfin/services/controllers/customer/cust_controller.dart';
 import 'package:url_launcher/url_launcher.dart' as UrlLauncher;
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class CustomerListWidget extends StatelessWidget {
-  CustomerListWidget(this._scaffoldKey, this.title, [this.userStatus = 0, this.fetchAll = true]);
+  CustomerListWidget(this._scaffoldKey, this.title,
+      [this.userStatus = 0, this.fetchAll = true]);
   final Customer _cust = Customer();
 
   final int userStatus;
@@ -34,128 +36,131 @@ class CustomerListWidget extends StatelessWidget {
                 shrinkWrap: true,
                 itemCount: snapshot.data.documents.length,
                 itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    color: CustomColors.mfinWhite,
-                    height: 150,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        ListTile(
-                          leading: Text(
-                            snapshot
-                                .data.documents[index].data['customer_name'],
-                            style: TextStyle(
-                              color: CustomColors.mfinBlue,
-                              fontSize: 18,
-                            ),
+                  return Slidable(
+                    actionPane: SlidableDrawerActionPane(),
+                    actionExtentRatio: 0.20,
+                    closeOnScroll: true,
+                    direction: Axis.horizontal,
+                    actions: <Widget>[
+                      IconSlideAction(
+                        caption: 'Phone',
+                        color: CustomColors.mfinPositiveGreen,
+                        icon: Icons.call,
+                        onTap: () => _makePhoneCall(snapshot
+                            .data.documents[index].data['mobile_number']),
+                      ),
+                      IconSlideAction(
+                        caption: 'Message',
+                        color: CustomColors.mfinGrey,
+                        icon: Icons.message,
+                        onTap: () => _makeSMS(snapshot
+                            .data.documents[index].data['mobile_number']),
+                      ),
+                    ],
+                    secondaryActions: <Widget>[
+                      IconSlideAction(
+                        caption: 'Edit',
+                        color: CustomColors.mfinPositiveGreen,
+                        icon: Icons.edit,
+                        onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => EditCustomerProfile(
+                                snapshot.data.documents[index].data),
                           ),
-                          title: Text(
-                            snapshot.data.documents[index].data['mobile_number']
-                                .toString(),
-                            style: TextStyle(
-                              color: CustomColors.mfinBlue,
-                              fontSize: 18,
-                            ),
-                          ),
-                          onTap: () {},
-                          trailing: IconButton(
-                            icon: Icon(
-                              Icons.edit,
-                              size: 35.0,
-                              color: CustomColors.mfinBlue,
-                            ),
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => EditCustomerProfile(
-                                      snapshot.data.documents[index].data),
+                        ),
+                      ),
+                      IconSlideAction(
+                        caption: 'Remove',
+                        color: CustomColors.mfinAlertRed,
+                        icon: Icons.delete_forever,
+                        onTap: () async {
+                          var state = Slidable.of(context);
+                          var dismiss = await showDialog<bool>(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: new Text(
+                                  "Confirm!",
+                                  style: TextStyle(
+                                      color: CustomColors.mfinAlertRed,
+                                      fontSize: 25.0,
+                                      fontWeight: FontWeight.bold),
+                                  textAlign: TextAlign.start,
                                 ),
+                                content: Text(
+                                    'Are you sure to remove ${snapshot.data.documents[index].data['customer_name']} Customer?'),
+                                actions: <Widget>[
+                                  FlatButton(
+                                    color: CustomColors.mfinButtonGreen,
+                                    child: Text(
+                                      "NO",
+                                      style: TextStyle(
+                                          color: CustomColors.mfinBlue,
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold),
+                                      textAlign: TextAlign.start,
+                                    ),
+                                    onPressed: () => Navigator.pop(context),
+                                  ),
+                                  FlatButton(
+                                      color: CustomColors.mfinAlertRed,
+                                      child: Text(
+                                        "YES",
+                                        style: TextStyle(
+                                            color: CustomColors.mfinLightGrey,
+                                            fontSize: 18.0,
+                                            fontWeight: FontWeight.bold),
+                                        textAlign: TextAlign.start,
+                                      ),
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                        _removeCustomer(
+                                            snapshot.data.documents[index]
+                                                .data['mobile_number']);
+                                      }),
+                                ],
                               );
                             },
-                          ),
-                        ),
-                        new Divider(
-                          color: CustomColors.mfinButtonGreen,
-                          thickness: 1,
-                        ),
-                        ListTile(
-                          leading: Container(
-                            height: 50,
-                            width: 130,
-                            child: RaisedButton.icon(
-                              color: CustomColors.mfinWhite,
-                              elevation: 5.0,
-                              onPressed: () => _makePhoneCall(
-                                snapshot.data.documents[index]
-                                    .data['mobile_number'],
-                              ),
-                              icon: Icon(
-                                Icons.call,
-                                size: 35.0,
-                                color: CustomColors.mfinPositiveGreen,
-                              ),
-                              label: Text(
-                                "Phone",
-                                style: TextStyle(
-                                  color: CustomColors.mfinBlue,
-                                  fontSize: 19,
-                                ),
+                          );
+
+                          if (dismiss != null && dismiss && state != null) {
+                            state.dismiss();
+                          }
+                        },
+                      ),
+                    ],
+                    child: Container(
+                      color: CustomColors.mfinWhite,
+                      height: 75,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          ListTile(
+                            leading: Text(
+                              snapshot
+                                  .data.documents[index].data['customer_name'],
+                              style: TextStyle(
+                                color: CustomColors.mfinBlue,
+                                fontSize: 18,
                               ),
                             ),
-                          ),
-                          title: Container(
-                            height: 50,
-                            width: 130,
-                            child: RaisedButton.icon(
-                              color: CustomColors.mfinWhite,
-                              elevation: 5.0,
-                              onPressed: () => _makeSMS(
-                                snapshot.data.documents[index]
-                                    .data['mobile_number'],
-                              ),
-                              icon: Icon(
-                                Icons.message,
-                                size: 40.0,
-                                color: CustomColors.mfinGrey,
-                              ),
-                              label: Text(
-                                "Text",
-                                style: TextStyle(
-                                  color: CustomColors.mfinBlue,
-                                  fontSize: 19,
-                                ),
+                            trailing: Text(
+                              snapshot
+                                  .data.documents[index].data['mobile_number']
+                                  .toString(),
+                              style: TextStyle(
+                                color: CustomColors.mfinBlue,
+                                fontSize: 18,
                               ),
                             ),
+                            onTap: () {},
                           ),
-                          onTap: () {},
-                          trailing: IconButton(
-                            icon: Icon(
-                              Icons.delete,
-                              size: 35.0,
-                              color: CustomColors.mfinAlertRed,
-                            ),
-                            onPressed: () {
-                              CustomDialogs.confirm(
-                                context,
-                                "Confirm",
-                                "Are you sure to remove ${snapshot.data.documents[index].data['customer_name']} Customer",
-                                () {
-                                  Navigator.pop(context);
-                                },
-                                () {
-                                  Navigator.pop(context);
-                                },
-                              );
-                              _removeCustomer(snapshot
-                                  .data.documents[index].data['mobile_number']);
-                            },
+                          new Divider(
+                            thickness: 2,
                           ),
-                        ),
-                        new Divider(
-                          thickness: 2,
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   );
                 },
@@ -274,13 +279,16 @@ class CustomerListWidget extends StatelessWidget {
   }
 
   _removeCustomer(int mobileNumber) async {
+    // CustomDialogs.actionWaiting(context, "Removing Customer");
     CustController _cc = CustController();
     var result = await _cc.removeCustomer(mobileNumber, false);
     if (result == null) {
+      // Navigator.pop(context);
       _scaffoldKey.currentState.showSnackBar(CustomSnackBar.errorSnackBar(
           "There are few Payments available for this Customer. Please remove the Payments first!",
           3));
     } else {
+      // Navigator.pop(context);
       _scaffoldKey.currentState.showSnackBar(
           CustomSnackBar.errorSnackBar("Customer removed successfully", 2));
     }
