8c84cd9fc2159ae37bfed19bfea7ad191ab7d1c0
vel <velcse46@gmail.com>
2020-05-23 23:36:46 +0530

    search bar suggestion fixes

--- lib/screens/app/SearchAppBar.dart
+++ lib/screens/app/SearchAppBar.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:instamfin/screens/customer/widgets/CustomerListTile.dart';
 import 'package:instamfin/screens/customer/widgets/CustomerListWidget.dart';
 import 'package:instamfin/screens/utils/CustomColors.dart';
 import 'package:instamfin/screens/utils/IconButton.dart';
+import 'package:instamfin/services/controllers/customer/cust_controller.dart';
 
 class SearchAppBar extends StatefulWidget {
   @override
   _SearchAppBarState createState() => new _SearchAppBarState();
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 }
+
+CustController _custController = CustController();
+int custNumber = 0;
+var custList;
 
 class _SearchAppBarState extends State<SearchAppBar> {
   @override
@@ -16,14 +22,25 @@ class _SearchAppBarState extends State<SearchAppBar> {
       appBar: new AppBar(
           backgroundColor: CustomColors.mfinBlue,
           centerTitle: true,
-          title: new TextField(
+          title: new TextFormField(
             style: new TextStyle(
               color: CustomColors.mfinWhite,
             ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter the customer number';
+              } else {
+                setState(() {
+                  custNumber = value.trim() as int;
+                });
+              }
+            },
             decoration: new InputDecoration(
                 prefixIcon: customIconButton(
                     Icons.search, 35.0, CustomColors.mfinWhite, () {
-                  CustomerListWidget(widget._scaffoldKey, "All", 0, false);
+                  setState(() {
+                    custList = _custController.searchCustomer(750275);
+                  });
                 }),
                 hintText: "Customer Search...",
                 hintStyle: new TextStyle(color: CustomColors.mfinWhite)),
@@ -31,6 +48,7 @@ class _SearchAppBarState extends State<SearchAppBar> {
           actions: <Widget>[
             customIconButton(Icons.close, 35.0, CustomColors.mfinWhite, () {}),
           ]),
+      body: customerListTile(context, custNumber, custList),
     );
   }
 }
