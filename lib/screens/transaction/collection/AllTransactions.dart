3700b7a37081d5d0a0b042171487617f27872797
vel <velcse46@gmail.com>
2020-05-25 21:50:41 +0530

    Collection book Tabview initial commit

---
+++ lib/screens/transaction/collection/AllTransactions.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:instamfin/db/models/payment.dart';
+import 'package:instamfin/db/models/user.dart';
+import 'package:instamfin/screens/utils/AsyncWidgets.dart';
+import 'package:instamfin/screens/utils/CustomColors.dart';
+import 'package:instamfin/services/controllers/transaction/payment_controller.dart';
+import 'package:instamfin/services/controllers/user/user_controller.dart';
+
+class AllTransactionsBuilder extends StatelessWidget {
+    AllTransactionsBuilder(this.startDate, this.endDate);
+
+  final PaymentController _pc = PaymentController();
+  final User _user = UserController().getCurrentUser();
+  final PaymentController _paymentController = PaymentController();
+
+  var startDate;
+  var endDate;
+  var totalCollectionAmount=0;
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Payment>>(
+      future: _paymentController.getAllPaymentsByDateRage(_user.primaryFinance,
+          _user.primaryBranch, _user.primarySubBranch, startDate, endDate),
+      builder: (BuildContext context, AsyncSnapshot<List<Payment>> snapshot) {
+        Widget widget;
+        if (snapshot.hasData) {
+            ListView.builder(
+                itemCount: snapshot.data.length,
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  Payment payment = snapshot.data[index];
+                  totalCollectionAmount = totalCollectionAmount + payment.collectionAmount;
+                }
+              );
+              widget = Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 30,
+                  child: ListTile(
+                    leading: Text(
+                      "Total Amount: ",
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: CustomColors.mfinBlue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    trailing: Text(totalCollectionAmount.toString(),
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: CustomColors.mfinBlue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+              );
+        } else if (snapshot.hasError) {
+          widget = Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: AsyncWidgets.asyncError(),
+          );
+        } else {
+          widget = Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: AsyncWidgets.asyncWaiting(),
+          );
+        }
+
+        return widget;
+      },
+    );
+  }
+}
--- lib/screens/transaction/collection/CollectionBookHome.dart
+++ lib/screens/transaction/collection/CollectionBookHome.dart
@@ -1,28 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:instamfin/screens/transaction/collection/PaymentListTile.dart';
+import 'package:instamfin/screens/transaction/collection/PaymentReports.dart';
 import 'package:instamfin/screens/transaction/collection/PaymentTemplatesScreen.dart';
-import 'package:instamfin/screens/transaction/widgets/TransactionsAppBar.dart';
 import 'package:instamfin/screens/utils/CustomColors.dart';
 
 class CollectionBookHome extends StatelessWidget {
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-
+  DateTime currentDate = DateTime.now();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        key: _scaffoldKey,
-        appBar: transactionsAppBar(
-            context,
-            "Collection Book",
-            '/transactions/collectionbook/template',
-            PaymentTemplateScreen()),
-        body: Center(
-            child: Text(
-          "TODO",
-          style: TextStyle(
-            color: CustomColors.mfinAlertRed,
-            fontSize: 18.0,
-            fontWeight: FontWeight.bold,
-          ),
-        )));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Collection Book"),
+          backgroundColor: CustomColors.mfinBlue,
+          actions: <Widget>[
+            IconButton(
+              alignment: Alignment.centerLeft,
+              iconSize: 40,
+              highlightColor: CustomColors.mfinWhite,
+              color: CustomColors.mfinFadedButtonGreen,
+              splashColor: CustomColors.mfinWhite,
+              icon: Icon(Icons.category),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => PaymentTemplateScreen(),
+                    settings: RouteSettings(
+                        name: '/transactions/collectionbook/template'),
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+        body: DefaultTabController(
+            length: 3,
+            child: Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.white,
+                elevation: 0,
+                bottom: TabBar(
+                    unselectedLabelColor: CustomColors.mfinBlue,
+                    indicatorSize: TabBarIndicatorSize.tab,
+                    indicator: BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                        CustomColors.mfinBlue,
+                        CustomColors.mfinLightBlue
+                      ]),
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    tabs: [
+                      Tab(
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text("Collections"),
+                        ),
+                      ),
+                      Tab(
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text("Payments"),
+                        ),
+                      ),
+                      Tab(
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text("All Transactions"),
+                        ),
+                      ),
+                    ]),
+              ),
+              body: TabBarView(children: [
+                PaymentListWidget(currentDate, new DateTime(currentDate.year, currentDate.month,
+                        currentDate.day+1)),
+                PaymentListWidget(new DateTime(currentDate.year, currentDate.month,
+                        currentDate.day-7), currentDate),
+                PaymentReportScreen(),
+              ]),
+            )),
+      ),
+    );
   }
 }
