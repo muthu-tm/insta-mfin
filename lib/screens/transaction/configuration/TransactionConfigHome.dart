561246c9efd89a75aad79c30ae0355a08a0eaac7
madhank93 <maddymadhan946@gmail.com>
2020-06-24 05:15:16 +0530

    Added action bar for Transaction config screen

--- lib/screens/transaction/configuration/TransactionConfigHome.dart
+++ lib/screens/transaction/configuration/TransactionConfigHome.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:instamfin/screens/home/Home.dart';
+import 'package:instamfin/screens/transaction/add/AddExpenseCategory.dart';
+import 'package:instamfin/screens/transaction/add/AddJournalCategory.dart';
+import 'package:instamfin/screens/transaction/add/AddPaymentTemplate.dart';
 import 'package:instamfin/screens/transaction/configuration/ExpenseCategoryListWidget.dart';
 import 'package:instamfin/screens/transaction/configuration/JournalCategoryListWidget.dart';
 import 'package:instamfin/screens/transaction/configuration/PaymentTemplateListWidget.dart';
 import 'package:instamfin/screens/utils/CustomColors.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class TransactionConfigHome extends StatelessWidget {
   @override
@@ -11,6 +16,99 @@ class TransactionConfigHome extends StatelessWidget {
       appBar: AppBar(
         title: Text('Transaction Configurations'),
         backgroundColor: CustomColors.mfinBlue,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showMaterialModalBottomSheet(
+            expand: false,
+            context: context,
+            backgroundColor: Colors.transparent,
+            builder: (context, scrollController) {
+              return Material(
+                  child: SafeArea(
+                top: false,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      title: Text('Add Payment template'),
+                      leading: Icon(
+                        Icons.monetization_on,
+                        color: CustomColors.mfinBlue,
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AddPaymentTemplate(),
+                            settings: RouteSettings(
+                                name:
+                                    '/transactions/collectionbook/template/add'),
+                          ),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      title: Text('Add Expense Categories'),
+                      leading: Icon(
+                        Icons.shopping_basket,
+                        color: CustomColors.mfinBlue,
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AddExpenseCategory(),
+                            settings: RouteSettings(
+                                name: '/transactions/expenses/categories/add'),
+                          ),
+                        );
+                      },
+                    ),
+                    ListTile(
+                        title: Text('Add Journal Categories'),
+                        leading: Icon(
+                          Icons.library_books,
+                          color: CustomColors.mfinBlue,
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => AddJournalCategory(),
+                              settings: RouteSettings(
+                                  name: '/transactions/journal/categories/add'),
+                            ),
+                          );
+                        }),
+                    ListTile(
+                      title: Text('Home'),
+                      leading: Icon(
+                        Icons.home,
+                        color: CustomColors.mfinBlue,
+                      ),
+                      onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => UserHomeScreen(),
+                          settings: RouteSettings(name: '/home'),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ));
+            },
+          );
+        },
+        backgroundColor: CustomColors.mfinBlue,
+        splashColor: CustomColors.mfinWhite,
+        child: Icon(
+          Icons.navigation,
+          size: 30,
+          color: CustomColors.mfinButtonGreen,
+        ),
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -107,6 +205,7 @@ class TransactionConfigHome extends StatelessWidget {
                 ],
               ),
             ),
+            Padding(padding: EdgeInsets.fromLTRB(0, 40, 0, 30))
           ],
         ),
       ),
