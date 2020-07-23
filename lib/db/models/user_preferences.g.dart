165913e216ef27b95c28c62ec60e3b8d1f7a69e0
Muthu Thavamani <muthucs@outlook.com>
2020-07-23 18:24:34 +0530

    user preference schema updated for fingerprint auth

--- lib/db/models/user_preferences.g.dart
+++ lib/db/models/user_preferences.g.dart
@@ -3,13 +3,15 @@ part of 'user_preferences.dart';
 UserPreferences _$UserPreferencesFromJson(Map<String, dynamic> json) {
   return UserPreferences()
     ..transactionGroupBy = json['transaction_group_by'] as int ?? 0
-    ..prefLanguage = json['support_language'] as String ?? 'English'
+    ..prefLanguage = json['language'] as String ?? 'English'
+    ..isfingerAuthEnabled = json['enable_fingerprint_auth'] as bool ?? false
     ..tableView = json['enable_table_view'] as bool ?? false;
 }
 
 Map<String, dynamic> _$UserPreferencesToJson(UserPreferences instance) =>
     <String, dynamic>{
       'transaction_group_by': instance.transactionGroupBy ?? 0,
-      'support_language': instance.prefLanguage ?? 'English',
+      'language': instance.prefLanguage ?? 'English',
+      'enable_fingerprint_auth': instance.isfingerAuthEnabled ?? false,
       'enable_table_view': instance.tableView ?? false
     };
