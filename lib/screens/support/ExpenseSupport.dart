1c7790d12c3a98f2031c77210cea3ed23cfd804d
madhank93 <maddymadhan946@gmail.com>
2020-09-02 23:52:27 +0530

    Completed support screen for - edit/remove expense

--- lib/screens/support/ExpenseSupport.dart
+++ lib/screens/support/ExpenseSupport.dart
@@ -158,7 +158,7 @@ class ExpenseSupport extends StatelessWidget {
                     ),
                     RichText(
                       text: TextSpan(
-                        text: "C: ",
+                        text: "D: ",
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           color: CustomColors.mfinAlertRed,
@@ -192,7 +192,7 @@ class ExpenseSupport extends StatelessWidget {
                     ),
                     RichText(
                       text: TextSpan(
-                        text: "D: ",
+                        text: "E: ",
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           color: CustomColors.mfinAlertRed,
@@ -216,6 +216,396 @@ class ExpenseSupport extends StatelessWidget {
                           ),
                           TextSpan(
                             text: " button to apply the changes",
+                            style: TextStyle(
+                              color: CustomColors.mfinBlack,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(5),
+                child: Row(
+                  children: [
+                    Text(
+                      "2. ",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: CustomColors.mfinBlack,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    Text(
+                      "Edit Expense:",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: CustomColors.mfinBlue,
+                        fontSize: 15.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding:
+                    EdgeInsets.only(left: 25, top: 10, right: 15, bottom: 10),
+                color: CustomColors.mfinLightGrey,
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        text: "A: ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: CustomColors.mfinAlertRed,
+                          fontSize: 18.0,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: "To edit ",
+                            style: TextStyle(
+                              color: CustomColors.mfinBlack,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "Expense",
+                            style: TextStyle(
+                              fontFamily: 'Georgia',
+                              color: CustomColors.mfinBlue,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: ", go to ",
+                            style: TextStyle(
+                              color: CustomColors.mfinBlack,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "Transactions -> Expense",
+                            style: TextStyle(
+                              fontFamily: 'Georgia',
+                              color: CustomColors.mfinBlue,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        text: "B: ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: CustomColors.mfinAlertRed,
+                          fontSize: 18.0,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: "Slide the ",
+                            style: TextStyle(
+                              color: CustomColors.mfinBlack,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "Expense title",
+                            style: TextStyle(
+                              fontFamily: 'Georgia',
+                              color: CustomColors.mfinBlue,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: " from right to left",
+                            style: TextStyle(
+                              color: CustomColors.mfinBlack,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        text: "C: ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: CustomColors.mfinAlertRed,
+                          fontSize: 18.0,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: "Click on ",
+                            style: TextStyle(
+                              color: CustomColors.mfinBlack,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "Edit",
+                            style: TextStyle(
+                              fontFamily: 'Georgia',
+                              color: CustomColors.mfinBlue,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: " button",
+                            style: TextStyle(
+                              color: CustomColors.mfinBlack,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        text: "D: ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: CustomColors.mfinAlertRed,
+                          fontSize: 18.0,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: "Update the ",
+                            style: TextStyle(
+                              color: CustomColors.mfinBlack,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "Expense",
+                            style: TextStyle(
+                              fontFamily: 'Georgia',
+                              color: CustomColors.mfinBlue,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: " details",
+                            style: TextStyle(
+                              color: CustomColors.mfinBlack,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        text: "E: ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: CustomColors.mfinAlertRed,
+                          fontSize: 18.0,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: "Press ",
+                            style: TextStyle(
+                              color: CustomColors.mfinBlack,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "Save",
+                            style: TextStyle(
+                              fontFamily: 'Georgia',
+                              color: CustomColors.mfinBlue,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: " button to apply the changes",
+                            style: TextStyle(
+                              color: CustomColors.mfinBlack,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(5),
+                child: Row(
+                  children: [
+                    Text(
+                      "3. ",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: CustomColors.mfinBlack,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    Text(
+                      "Remove Expense:",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: CustomColors.mfinBlue,
+                        fontSize: 15.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding:
+                    EdgeInsets.only(left: 25, top: 10, right: 15, bottom: 10),
+                color: CustomColors.mfinLightGrey,
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        text: "A: ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: CustomColors.mfinAlertRed,
+                          fontSize: 18.0,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: "To remove ",
+                            style: TextStyle(
+                              color: CustomColors.mfinBlack,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "Expense",
+                            style: TextStyle(
+                              fontFamily: 'Georgia',
+                              color: CustomColors.mfinBlue,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: ", go to ",
+                            style: TextStyle(
+                              color: CustomColors.mfinBlack,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "Transactions -> Expense",
+                            style: TextStyle(
+                              fontFamily: 'Georgia',
+                              color: CustomColors.mfinBlue,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        text: "B: ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: CustomColors.mfinAlertRed,
+                          fontSize: 18.0,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: "Slide the ",
+                            style: TextStyle(
+                              color: CustomColors.mfinBlack,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "Expense title",
+                            style: TextStyle(
+                              fontFamily: 'Georgia',
+                              color: CustomColors.mfinBlue,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: " from left to right",
+                            style: TextStyle(
+                              color: CustomColors.mfinBlack,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        text: "C: ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: CustomColors.mfinAlertRed,
+                          fontSize: 18.0,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: "Click on ",
+                            style: TextStyle(
+                              color: CustomColors.mfinBlack,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "Remove",
+                            style: TextStyle(
+                              fontFamily: 'Georgia',
+                              color: CustomColors.mfinBlue,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: " button",
+                            style: TextStyle(
+                              color: CustomColors.mfinBlack,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        text: "D: ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: CustomColors.mfinAlertRed,
+                          fontSize: 18.0,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: "Press ",
+                            style: TextStyle(
+                              color: CustomColors.mfinBlack,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "Yes",
+                            style: TextStyle(
+                              fontFamily: 'Georgia',
+                              color: CustomColors.mfinBlue,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: " button in the popup to remove the expense",
                             style: TextStyle(
                               color: CustomColors.mfinBlack,
                               fontSize: 13.0,
